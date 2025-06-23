4113dcbb1be2ae75802ded437e0e3c0feeb31780
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-24 02:19:29 +0300

    clippy fixes

--- crates/evops-uniffi/src/conversions.rs
+++ crates/evops-uniffi/src/conversions.rs
@@ -1,28 +1,29 @@
 impl<T> From<Result<T, evops_models::UserNameError>> for crate::ValidateUserNameResult {
     fn from(value: Result<T, evops_models::UserNameError>) -> Self {
-        use crate::ValidateUserNameResult as B;
-        use evops_models::UserNameError as N;
         match value {
-            Ok(_) => B::Ok,
-            Err(e) => match e {
-                N::LenCharMaxViolated => B::LenCharMaxViolated,
-                N::NotEmptyViolated => B::NotEmptyViolated,
-            },
+            Ok(_) => Self::Ok,
+            Err(e) => {
+                use evops_models::UserNameError as E;
+                match e {
+                    E::LenCharMaxViolated => Self::LenCharMaxViolated,
+                    E::NotEmptyViolated => Self::NotEmptyViolated,
+                }
+            }
         }
     }
 }
 
 impl<T> From<Result<T, evops_models::EventTitleError>> for crate::ValidateEventTitleResult {
     fn from(value: Result<T, evops_models::EventTitleError>) -> Self {
-        use crate::ValidateEventTitleResult as B;
-        use evops_models::EventTitleError as N;
-
         match value {
-            Ok(_) => B::Ok,
-            Err(e) => match e {
-                N::LenCharMaxViolated => B::LenCharMaxViolated,
-                N::NotEmptyViolated => B::NotEmptyViolated,
-            },
+            Ok(_) => Self::Ok,
+            Err(e) => {
+                use evops_models::EventTitleError as E;
+                match e {
+                    E::LenCharMaxViolated => Self::LenCharMaxViolated,
+                    E::NotEmptyViolated => Self::NotEmptyViolated,
+                }
+            }
         }
     }
 }
@@ -31,45 +32,44 @@ impl<T> From<Result<T, evops_models::EventDescriptionError>>
     for crate::ValidateEventDescriptionResult
 {
     fn from(value: Result<T, evops_models::EventDescriptionError>) -> Self {
-        use crate::ValidateEventDescriptionResult as B;
-        use evops_models::EventDescriptionError as N;
-
         match value {
-            Ok(_) => B::Ok,
-            Err(e) => match e {
-                N::LenCharMaxViolated => B::LenCharMaxViolated,
-                N::NotEmptyViolated => B::NotEmptyViolated,
-            },
+            Ok(_) => Self::Ok,
+            Err(e) => {
+                use evops_models::EventDescriptionError as E;
+                match e {
+                    E::LenCharMaxViolated => Self::LenCharMaxViolated,
+                    E::NotEmptyViolated => Self::NotEmptyViolated,
+                }
+            }
         }
     }
 }
 
 impl<T> From<Result<T, evops_models::TagNameError>> for crate::ValidateTagNameResult {
     fn from(value: Result<T, evops_models::TagNameError>) -> Self {
-        use crate::ValidateTagNameResult as B;
-        use evops_models::TagNameError as N;
-
         match value {
-            Ok(_) => B::Ok,
-            Err(e) => match e {
-                N::LenCharMaxViolated => B::LenCharMaxViolated,
-                N::NotEmptyViolated => B::NotEmptyViolated,
-                N::RegexViolated => B::RegexViolated,
-            },
+            Ok(_) => Self::Ok,
+            Err(e) => {
+                use evops_models::TagNameError as E;
+                match e {
+                    E::LenCharMaxViolated => Self::LenCharMaxViolated,
+                    E::NotEmptyViolated => Self::NotEmptyViolated,
+                    E::RegexViolated => Self::RegexViolated,
+                }
+            }
         }
     }
 }
 
 impl<T> From<Result<T, evops_models::TagAliasError>> for crate::ValidateTagAliasResult {
     fn from(value: Result<T, evops_models::TagAliasError>) -> Self {
-        use crate::ValidateTagAliasResult as B;
         use evops_models::TagAliasError as N;
 
         match value {
-            Ok(_) => B::Ok,
+            Ok(_) => Self::Ok,
             Err(e) => match e {
-                N::LenCharMaxViolated => B::LenCharMaxViolated,
-                N::NotEmptyViolated => B::NotEmptyViolated,
+                N::LenCharMaxViolated => Self::LenCharMaxViolated,
+                N::NotEmptyViolated => Self::NotEmptyViolated,
             },
         }
     }
