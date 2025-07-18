1be4f7f34a559bbf2090e712795c2bdc11c617d8
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-18 18:27:55 +0300

    .

--- crates/evops-models/src/services/user.rs
+++ crates/evops-models/src/services/user.rs
@@ -1,28 +1,57 @@
+use std::sync::LazyLock;
+
 use nutype::nutype;
+use regex::Regex;
 use uuid::Uuid;
 
 #[derive(Debug)]
 pub struct NewUserForm {
-    pub name: crate::UserName,
+    pub login: UserLogin,
+    pub display_name: UserDisplayName,
 }
 
 #[derive(Debug)]
 pub struct User {
     pub id: crate::UserId,
-    pub name: crate::UserName,
+    pub login: UserLogin,
+    pub display_name: UserDisplayName,
 }
 
 #[nutype(derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Display))]
 pub struct UserId(Uuid);
 
-pub const USER_NAME_MIN_LEN: usize = 1;
-pub const USER_NAME_MAX_LEN: usize = 64;
+pub const USER_DISPLAY_NAME_MIN_LEN: usize = 1;
+pub const USER_DISPLAY_NAME_MAX_LEN: usize = 64;
 #[nutype(
     new_unchecked,
-    validate(len_char_max = crate::USER_NAME_MAX_LEN, not_empty),
+    validate(len_char_min = USER_DISPLAY_NAME_MIN_LEN, len_char_max = USER_DISPLAY_NAME_MAX_LEN),
     derive(Debug, PartialEq, Eq, AsRef, Hash),
 )]
-pub struct UserName(String);
+pub struct UserDisplayName(String);
+
+pub const USER_LOGIN_MIN_LEN: usize = 4;
+pub const USER_LOGIN_MAX_LEN: usize = 32;
+pub static USER_LOGIN_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new("^.*$").unwrap());
+#[nutype(
+    new_unchecked,
+    validate(
+        len_char_min = USER_LOGIN_MIN_LEN,
+        len_char_max = USER_LOGIN_MAX_LEN,
+        regex = USER_LOGIN_REGEX,
+    ),
+    derive(Debug, PartialEq, Eq, AsRef, Hash),
+)]
+pub struct UserLogin(String);
+
+pub const USER_PASSWORD_MIN_LEN: usize = 8;
+pub const USER_PASSWORD_MAX_LEN: usize = 64;
+pub static USER_PASSWORD_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new("^.*$").unwrap());
+#[nutype(validate(
+    len_char_min = USER_PASSWORD_MIN_LEN,
+    len_char_max = USER_PASSWORD_MAX_LEN,
+    regex = USER_PASSWORD_REGEX,
+))]
+pub struct UserPassword(String);
 
 #[allow(clippy::repeat_once)]
 #[cfg(test)]
@@ -30,13 +59,13 @@ mod tests {
     #[test]
     fn user_name() {
         assert_eq!(
-            crate::UserName::try_new(""),
+            crate::UserDisplayName::try_new(""),
             Err(crate::UserNameError::NotEmptyViolated),
         );
-        assert!(crate::UserName::try_new("a".repeat(1)).is_ok());
-        assert!(crate::UserName::try_new("a".repeat(64)).is_ok());
+        assert!(crate::UserDisplayName::try_new("a".repeat(1)).is_ok());
+        assert!(crate::UserDisplayName::try_new("a".repeat(64)).is_ok());
         assert_eq!(
-            crate::UserName::try_new("a".repeat(65)),
+            crate::UserDisplayName::try_new("a".repeat(65)),
             Err(crate::UserNameError::LenCharMaxViolated),
         );
     }
