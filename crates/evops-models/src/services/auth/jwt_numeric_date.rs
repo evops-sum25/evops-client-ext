dfd529f419bb79759157e62843113d3f7a077d35
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-20 02:33:17 +0300

    .

--- crates/evops-models/src/services/auth/jwt_numeric_date.rs
+++ crates/evops-models/src/services/auth/jwt_numeric_date.rs
@@ -1,8 +1,9 @@
-//! Custom serialization of OffsetDateTime to conform with the JWT spec (RFC 7519 section 2, "Numeric Date")
+//! Custom serialization of `DateTime<Utc>`  to conform with the JWT spec (RFC 7519 section 2, "Numeric Date").
+
 use chrono::{DateTime, Utc};
 use serde::{self, Deserialize, Deserializer, Serializer};
 
-/// Serializes an OffsetDateTime to a Unix timestamp (milliseconds since 1970/1/1T00:00:00T)
+/// Serializes a `DateTime<Utc>` to a Unix timestamp (milliseconds since 1970/1/1T00:00:00T).
 pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
@@ -11,11 +12,11 @@ where
     serializer.serialize_i64(timestamp)
 }
 
-/// Attempts to deserialize an i64 and use as a Unix timestamp
+/// Attempts to deserialize an i64 and use as a Unix timestamp.
 pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
 where
     D: Deserializer<'de>,
 {
     DateTime::from_timestamp(i64::deserialize(deserializer)?, 0)
-        .ok_or(serde::de::Error::custom("invalid Unix timestamp value"))
+        .ok_or_else(|| serde::de::Error::custom("invalid Unix timestamp value"))
 }
